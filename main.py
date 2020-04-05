250bfb20b156cd27e9ee07f74249664d7d69f444
Maciek_Markul <m.markul1337@gmail.com>
2020-04-05 22:37:44 +0200

    fsdcd

--- main.py
+++ main.py
@@ -5,20 +5,8 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class PatientRq(BaseModel):
-    name: str
-    surename: str
-
-class PatientResp(BaseModel):
-    id: int = 1
-    patient: Dict
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-
-
-@app.post("/patient")
-def create_patient(rq: PatientRq):
-    return PatientResp(patient=rq.dict())
 
