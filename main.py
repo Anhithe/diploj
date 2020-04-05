87ad99ed4bd4a6e4a2033304173046d5b89c3f7a
Maciek_Markul <m.markul1337@gmail.com>
2020-04-05 22:31:43 +0200

    ux s

--- main.py
+++ main.py
@@ -5,26 +5,6 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-@app.get("/method")
-def request_type():
-    return {"method": "GET"}
-
-@app.post("/method")
-def request_type():
-    return {"method": "POST"}
-
-@app.delete("/method")
-def request_type():
-    return {"method": "DELETE"}
-
-@app.put("/method")
-def request_type():
-    return {"method": "PUT"}
-
 class PatientRq(BaseModel):
     name: str
     surename: str
