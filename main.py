2ada4b69e39c7aafa0a5c5d2cb5fd152f9d2b761
Anhithe <m.markul1337@gmail.com>
2020-04-27 01:17:40 +0200

    xbhjasjksakj

--- main.py
+++ main.py
@@ -1,44 +1,20 @@
 # main.py
 
-from fastapi import FastAPI, HTTPException, Cookie, Response
+from fastapi import FastAPI, HTTPException, Cookie, Response, Depends
 from pydantic import BaseModel
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 app.counter = 0
-app.secret_key = "very constant and random secret, best 64 characters"
-
-class PatientRq(BaseModel):
-    name: str
-    surename: str
-
-class PatientResp(BaseModel):
-    id: str
-    patient: dict
-
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-@app.get('/num/{p}')
-def counter(p):
-    return str(p)
-
 @app.get("/welcome")
 def txt():
     return "no elo"
 
-@app.post("/patient")
-def create_patient(rq: PatientRq):
-    app.counter += 1
-    return PatientResp(id=str(app.counter), patient=rq.dict())
-
-@app.get("/patient/{pk}")
-def patient_finder(pk):
-    if int(pk) > app.counter:
-        raise HTTPException(status_code=204, detail="No content")
-    return PatientRq(name="NAME", surename="SURENAME")
 
 security = HTTPBasic()
 @app.get("/login")
