b076229272c59ccdd65890bf1f9ad4445d1b483f
Anhithe <m.markul1337@gmail.com>
2020-05-04 01:34:08 +0200

    des

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 from fastapi import FastAPI, HTTPException
+import pytest
 
 app = FastAPI()
 
@@ -14,11 +15,13 @@ async def shutdown():
     app.db_connection.close()
 
 
-@app.get("/tracks/composers")
+@app.get("/tracks/composers/")
 async def composer(composer_name: str):
     cursor = app.db_connection.cursor()
     app.db_connection.row_factory = lambda cursor, x: x[0]
     tracks = cursor.execute("SELECT Name FROM tracks WHERE Composer LIKE ? ORDER BY Name", ("%"+composer_name+"%", )).fetchall()
     if not tracks:
         raise HTTPException(status_code=404, detail="error")
-    return tracks
+    return {
+        "composer": composer_name,
+    }
