adbc9be78566722681d04d55a79ca2bae6bd42af
Anhithe <m.markul1337@gmail.com>
2020-05-03 17:57:26 +0200

    adm:

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sqlite3
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -15,13 +14,18 @@ async def shutdown():
     app.db_connection.close()
 
 
-@app.get("/tracks")
-def root():
-    return "Welcome to the jungle"
+@app.get("/tracks", status_code=200)
+async def tracker(page: int = 0, per_page: int = 10):
+    page = page*per_page
+    app.db_connection.row_factory = sqlite3.Row
+    tracks = app.db_connection.execute(
+        "SELECT * FROM tracks ORDER BY TrackId LIMIT :per_page OFFSET :page",
+        {'per_page': per_page, 'page': page}).fetchall()
+    return tracks
 
 
 @app.get("/tracks/{page}/{per_page}", status_code=200)
-async def root(page: int = 0, per_page: int = 10):
+async def tracker(page: int = 0, per_page: int = 10):
     page = page*per_page
     app.db_connection.row_factory = sqlite3.Row
     tracks = app.db_connection.execute(
