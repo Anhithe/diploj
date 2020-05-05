950b72f99d2dc6125a97d5e40498e046d08ef490
Anhithe <m.markul1337@gmail.com>
2020-05-05 03:28:53 +0200

    adjbk

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ class Album(BaseModel):
     artist_id: int
 
 
+class Customer(BaseModel):
+    company: str
+    address: str
+    city: str
+    state: str
+    country: str
+    postalcode: str
+    fax: str
+
+
 @app.on_event("startup")
 async def startup():
     app.db_connection = sqlite3.connect('chinook.db')
@@ -24,7 +34,8 @@ async def shutdown():
 async def composer(composer_name: str):
     cursor = app.db_connection.cursor()
     app.db_connection.row_factory = lambda cursor, x: x[0]
-    tracks = cursor.execute("SELECT Name FROM tracks WHERE Composer LIKE ? ORDER BY Name", ("%"+composer_name+"%", )).fetchall()
+    tracks = cursor.execute("SELECT Name FROM tracks WHERE Composer LIKE ? ORDER BY Name",
+                            ("%" + composer_name + "%",)).fetchall()
     if not tracks:
         raise HTTPException(status_code=404, detail="error")
     return tracks
@@ -32,7 +43,8 @@ async def composer(composer_name: str):
 
 @app.post("/albums/", status_code=201)
 async def albums_add(album: Album):
-    checker = app.db_connection.execute("SELECT ArtistId FROM artists WHERE ArtistId = ?", (album.artist_id,)).fetchone()
+    checker = app.db_connection.execute("SELECT ArtistId FROM artists WHERE ArtistId = ?",
+                                        (album.artist_id,)).fetchone()
     if not checker:
         raise HTTPException(status_code=404, detail="error")
     cursor = app.db_connection.execute(
@@ -51,3 +63,43 @@ async def album_getter(album_id: int):
     app.db_connection.row_factory = lambda cursor, x: x[0]
     album1 = cursor.execute("SELECT * FROM albums WHERE AlbumId = ?", (album_id,)).fetchone()
     return album1
+
+
+@app.put("/customers/{customer_id}", status_code=200)
+async def putter(customer_id: int, customer: Customer):
+    checker = app.db_connection.execute("SELECT CustomerId FROM customers WHERE CustomerId = ?",
+                                        (customer_id,)).fetchone()
+    if not checker:
+        raise HTTPException(status_code=404, detail="error")
+    app.db_connection.row_factory = sqlite3.Row
+    customer_data = app.db_connection.execute("SELECT * FROM customers WHERE CustomerId = ?",
+                                          (customer_id,)).fetchone()
+    update_customer = customer.dict(exclude_unset=True)
+    updated_customer = customer.copy(update=update_customer)
+    if updated_customer.company:
+        cursor = app.db_connection.execute("UPDATE Customers SET company = ? WHERE CustomerId = ?",
+                                       (updated_customer.company, customer_id))
+    if updated_customer.address:
+        cursor = app.db_connection.execute("UPDATE Customers SET address = ? WHERE CustomerId = ?",
+                                       (updated_customer.address, customer_id))
+    if updated_customer.city:
+        cursor = app.db_connection.execute("UPDATE Customers SET city = ? WHERE CustomerId = ?",
+                                       (updated_customer.city, customer_id))
+    if updated_customer.state:
+        cursor = app.db_connection.execute("UPDATE Customers SET state = ? WHERE CustomerId = ?",
+                                       (updated_customer.state, customer_id))
+    if updated_customer.country:
+        cursor = app.db_connection.execute("UPDATE Customers SET country = ? WHERE CustomerId = ?",
+                                       (updated_customer.country, customer_id))
+    if updated_customer.postalcode:
+        cursor = app.db_connection.execute("UPDATE Customers SET postalcode = ? WHERE CustomerId = ?",
+                                       (updated_customer.postalcode, customer_id))
+    if updated_customer.fax:
+        cursor = app.db_connection.execute("UPDATE Customers SET fax = ? WHERE CustomerId = ?",
+                                       (updated_customer.fax, customer_id))
+    app.db_connection.commit()
+
+    stored_customer_data = app.db_connection.execute("SELECT * FROM customers WHERE CustomerId = ?",
+                                                     (customer_id,)).fetchone()
+    return stored_customer_data
+
